423c175a38a315bfd2814f30a49028f3a1352c11
David Bazan <davidbazan@umail.ucsb.edu>
2025-11-03 12:04:46 -0800

    DB - Fixed format of UCSBOrganization fixture

--- frontend/src/fixtures/UCSBOrganizationFixtures.js
+++ frontend/src/fixtures/UCSBOrganizationFixtures.js
@@ -1,31 +1,30 @@
 const ucsbOrganizationFixtures = {
-    oneOrganization: {
-      orgCode: "AB",
-      orgTranslationShort: "Alpha Beta",
-      orgTranslation: "Alpha Beta",
+  oneOrganization: {
+    orgCode: "AB",
+    orgTranslationShort: "Alpha Beta",
+    orgTranslation: "Alpha Beta",
+    inactive: false,
+  },
+  threeOrganization: [
+    {
+      orgCode: "WPC",
+      orgTranslationShort: "Water Polo Cl",
+      orgTranslation: "Water Polo Club",
       inactive: false,
     },
-    threeOrganization: [
-      {
-        orgCode: "WPC",
-        orgTranslationShort: "Water Polo Cl",
-        orgTranslation: "Water Polo Club",
-        inactive: false,
-      },
-      {
-        orgCode: "AP",
-        orgTranslationShort: "Alpha Phi",
-        orgTranslation: "Alpha Phi",
-        inactive: true,
-      },
-      {
-        orgCode: "KRC",
-        orgTranslationShort: "Korean Radio Cl",
-        orgTranslation: "Korean Radio Club",
-        inactive: false,
-      },
-    ],
-  };
-  
-  export { ucsbOrganizationFixtures };
-  
+    {
+      orgCode: "AP",
+      orgTranslationShort: "Alpha Phi",
+      orgTranslation: "Alpha Phi",
+      inactive: true,
+    },
+    {
+      orgCode: "KRC",
+      orgTranslationShort: "Korean Radio Cl",
+      orgTranslation: "Korean Radio Club",
+      inactive: false,
+    },
+  ],
+};
+
+export { ucsbOrganizationFixtures };
