39172ee7fd0b11d4840e46f2d0491702f2a2ffae
Samprita Chakraborty <sampritac9@gmail.com>
2025-11-03 15:49:15 -0800

    added table + tests

---
+++ frontend/src/stories/components/UCSBDiningCommonsMenuItems/UCSBDiningCommonsMenuItemsTable.stories.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import UCSBDiningCommonsMenuItemsTable from "main/components/UCSBDiningCommonsMenuItems/UCSBDiningCommonsMenuItemsTable";
+import { UCSBDiningCommonsMenuItemsFixtures } from "fixtures/UCSBDiningCommonsMenuItems";
+import { currentUserFixtures } from "fixtures/currentUserFixtures";
+import { http, HttpResponse } from "msw";
+
+export default {
+  title: "components/UCSBDiningCommonsMenuItems/UCSBDiningCommonsMenuItemsTable",
+  component: UCSBDiningCommonsMenuItemsTable,
+};
+
+const Template = (args) => {
+  return <UCSBDiningCommonsMenuItemsTable {...args} />;
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+  items: [],
+};
+
+export const ThreeItemsOrdinaryUser = Template.bind({});
+
+ThreeItemsOrdinaryUser.args = {
+  items: UCSBDiningCommonsMenuItemsFixtures.threeItems,
+  currentUser: currentUserFixtures.userOnly,
+};
+
+export const ThreeItemsAdminUser = Template.bind({});
+ThreeItemsAdminUser.args = {
+  items: UCSBDiningCommonsMenuItemsFixtures.threeItems,
+  currentUser: currentUserFixtures.adminUser,
+};
+
+ThreeItemsAdminUser.parameters = {
+  msw: [
+    http.delete("/api/ucsbdiningcommons", () => {
+      return HttpResponse.json({}, { status: 200 });
+    }),
+  ],
+};
