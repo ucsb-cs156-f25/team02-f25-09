2f600247f9721eb57fb7f2c1c435299f8fb1bf5a
Viktor Minchev <vikitominchev@gmail.com>
2025-11-04 11:47:13 -0800

    feat: enhance ArticleForm with additional fields and validation; add tests for functionality

--- frontend/src/stories/components/Articles/ArticleForm.stories.jsx
+++ frontend/src/stories/components/Articles/ArticleForm.stories.jsx
@@ -24,7 +24,7 @@ Create.args = {
 export const Update = Template.bind({});
 
 Update.args = {
-  initialContents: articleFixtures,
+  initialContents: articleFixtures.oneArticle,
   buttonLabel: "Update",
   submitAction: (data) => {
     console.log("Submit was clicked with data: ", data);
