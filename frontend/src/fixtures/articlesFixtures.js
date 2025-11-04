71ea02555f00fa679d13e7e0580cb9bcb2419dda
Viktor Minchev <vikitominchev@gmail.com>
2025-11-03 20:01:35 -0800

    viktorinkov - add fixtures for article table

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,38 @@
+const commitFixtures = {
+  oneArticle: {
+    "id": 8,
+    "title": "All the right notes: the ENO’s ‘Dead Man Walking’",
+    "url": "https://www.thearticle.com/all-the-right-notes-the-enos-dead-man-walking",
+    "explanation": "This harrowing opera is based",
+    "email": "markronan@gmail.com",
+    "dateAdded": "2025-11-03T19:25:00"
+  },
+  threeArticles: [
+    {
+      "id": 8,
+      "title": "All the right notes: the ENO’s ‘Dead Man Walking’",
+      "url": "https://www.thearticle.com/all-the-right-notes-the-enos-dead-man-walking",
+      "explanation": "This harrowing opera is based",
+      "email": "markronan@gmail.com",
+      "dateAdded": "2025-11-03T19:25:00"
+    },
+    {
+      "id": 9,
+      "title": "Christopher Marlowe: the muses’ darling",
+      "url": "https://www.thearticle.com/christopher-marlowe-the-muses-darling",
+      "explanation": "Many fascinating secrets and mysteries surround the life of Christopher Marlowe",
+      "email": "jefreymayers@gmail.com",
+      "dateAdded": "2003-11-03T19:25:00"
+    },
+    {
+      "id": 10,
+      "title": "MSC CRUISES SERVES AS TITLE SPONSOR FOR AUSTIN 2025 GRAND PRIX™, ANNOUNCES CONTINUED PARTNERSHIP WITH FORMULA 1® FOR 2026 SEASON",
+      "url": "https://www.multivu.com/msc-cruises/9301852-en-msc-cruises-title-sponsor-of-austin-grand-prix-2025-2026",
+      "explanation": "One month ahead from the start of year-round sailings from Galveston, MSC Cruises served as title sponsor of FORMULA 1 MSC CRUISES UNITED STATES GRAND PRIX 2025 in Austin",
+      "email": "msdc@cruise.com",
+      "dateAdded": "2003-10-05T19:15:00"
+    }
+  ],
+};
+
+export { ucsbDatesFixtures };
