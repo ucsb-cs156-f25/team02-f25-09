f4435e0e811945ec8f0592d11c9b8e25c6aec053
Viktor Minchev <vikitominchev@gmail.com>
2025-11-04 13:09:35 -0800

    fix: remove duplicate import of articleFixtures in ArticleForm test

--- frontend/src/tests/components/Articles/ArticleForm.test.jsx
+++ frontend/src/tests/components/Articles/ArticleForm.test.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from "react-router";
 import ArticleForm from "main/components/Articles/ArticleForm";
 import { articleFixtures } from "fixtures/articleFixtures";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { articleFixtures } from "fixtures/articleFixtures";
 import { expect } from "vitest";
 
 const mockedNavigate = vi.fn();
