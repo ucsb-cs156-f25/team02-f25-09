465ce1948613f697ad80a1f0a988e47359693fd8
Dennis Kim <dkim876@ucsb.edu>
2025-11-03 23:06:04 -0800

    dk - add HelpRequestTable, tests and stories. also ran autoformatter

--- frontend/src/main/utils/helpRequestUtils.js
+++ frontend/src/main/utils/helpRequestUtils.js
@@ -7,7 +7,7 @@ export function onDeleteSuccess(message) {
 
 export function cellToAxiosParamsDelete(cell) {
   return {
-    url: "/api/ucsbdiningcommons",
+    url: "/api/helprequest",
     method: "DELETE",
     params: {
       id: cell.row.original.id,
