aa7fb4b6e49b5a00d79715f8d29c401140423650
David Bazan <davidbazan@umail.ucsb.edu>
2025-11-05 00:12:18 -0800

    DB-Organization Edits page and tests

--- frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationEditPage.test.jsx
+++ frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationEditPage.test.jsx
@@ -1,47 +1,173 @@
-import { render, screen } from "@testing-library/react";
-import UCSBOrganizationEditPage from "main/pages/UCSBOrganization/UCSBOrganizationEditPage";
+import { fireEvent, render, waitFor, screen } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MemoryRouter } from "react-router";
+import UCSBOrganizationEditPage from "main/pages/UCSBOrganization/UCSBOrganizationEditPage";
 
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
-import { expect } from "vitest";
+import mockConsole from "tests/testutils/mockConsole";
 
-describe("UCSBOrganizationEditPage tests", () => {
-  const axiosMock = new AxiosMockAdapter(axios);
-
-  const setupUserOnly = () => {
-    axiosMock.reset();
-    axiosMock.resetHistory();
-    axiosMock
-      .onGet("/api/currentUser")
-      .reply(200, apiCurrentUserFixtures.userOnly);
-    axiosMock
-      .onGet("/api/systemInfo")
-      .reply(200, systemInfoFixtures.showingNeither);
+const mockToast = vi.fn();
+vi.mock("react-toastify", async (importOriginal) => {
+  const originalModule = await importOriginal();
+  return {
+    ...originalModule,
+    toast: vi.fn((x) => mockToast(x)),
   };
+});
 
-  const queryClient = new QueryClient();
-  test("Renders expected content", async () => {
-    // arrange
+const mockNavigate = vi.fn();
+vi.mock("react-router", async (importOriginal) => {
+  const originalModule = await importOriginal();
+  return {
+    ...originalModule,
+    useParams: vi.fn(() => ({
+      orgCode: "DMB",
+    })),
+    Navigate: vi.fn((x) => {
+      mockNavigate(x);
+      return null;
+    }),
+  };
+});
 
-    setupUserOnly();
 
-    // act
-    render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-          <UCSBOrganizationEditPage />
-        </MemoryRouter>
-      </QueryClientProvider>,
-    );
 
-    // assert
-    await screen.findByText("Edit page not yet implemented");
-    expect(
-      screen.getByText("Edit page not yet implemented"),
-    ).toBeInTheDocument();
+let axiosMock;
+describe("UCSBOrganizationEditPage tests", () => {
+  describe("when the backend doesn't return data", () => {
+    beforeEach(() => {
+      axiosMock = new AxiosMockAdapter(axios);
+      axiosMock.reset();
+      axiosMock.resetHistory();
+      axiosMock
+        .onGet("/api/currentUser")
+        .reply(200, apiCurrentUserFixtures.userOnly);
+      axiosMock
+        .onGet("/api/systemInfo")
+        .reply(200, systemInfoFixtures.showingNeither);
+      axiosMock.onGet("/api/ucsborganization", { params: { orgCode: "DMB" } }).timeout();
+    });
+
+    afterEach(() => {
+      mockToast.mockClear();
+      mockNavigate.mockClear();
+      axiosMock.restore();
+      axiosMock.resetHistory();
+    });
+
+    const queryClient = new QueryClient();
+    test("renders header but table is not present", async () => {
+      const restoreConsole = mockConsole();
+
+      render(
+        <QueryClientProvider client={queryClient}>
+          <MemoryRouter>
+            <UCSBOrganizationEditPage />
+          </MemoryRouter>
+        </QueryClientProvider>,
+      );
+      await screen.findByText("Edit Organization");
+      expect(screen.queryByTestId("Organization-orgCode")).not.toBeInTheDocument();
+      restoreConsole();
+    });
+  });
+
+  describe("tests where backend is working normally", () => {
+    beforeEach(() => {
+      axiosMock = new AxiosMockAdapter(axios);
+      axiosMock.reset();
+      axiosMock.resetHistory();
+      axiosMock
+        .onGet("/api/currentUser")
+        .reply(200, apiCurrentUserFixtures.userOnly);
+      axiosMock
+        .onGet("/api/systemInfo")
+        .reply(200, systemInfoFixtures.showingNeither);
+      axiosMock.onGet("/api/ucsborganization", { params: { orgCode: "DMB" } }).reply(200, {
+        orgCode: "DMB",
+        orgTranslationShort: "Dean Map B",
+        orgTranslation: "Dean Map Bean",
+        inactive: false,
+      });
+      axiosMock.onPut("/api/ucsborganization").reply(200, {
+        orgCode: "DMB",
+        orgTranslationShort: "Dean MAGE B",
+        orgTranslation: "Dean MAGE Bean",
+        inactive: false,
+      });
+    });
+
+    afterEach(() => {
+      mockToast.mockClear();
+      mockNavigate.mockClear();
+      axiosMock.restore();
+      axiosMock.resetHistory();
+    });
+
+    const queryClient = new QueryClient();
+
+    test("Is populated with the data provided", async () => {
+      render(
+        <QueryClientProvider client={queryClient}>
+          <MemoryRouter>
+            <UCSBOrganizationEditPage />
+          </MemoryRouter>
+        </QueryClientProvider>,
+      );
+
+      await screen.findByTestId("UCSBOrganizationForm-orgCode");
+
+      const orgCodeField = screen.getByTestId("UCSBOrganizationForm-orgCode");
+      const orgTranslationShortField = screen.getByTestId("UCSBOrganizationForm-orgTranslationShort");
+      const orgTranslationField = screen.getByLabelText("Org Translation");
+      const inactiveField = screen.getByLabelText("Inactive");
+      const submitButton = screen.getByText("Update");
+
+      expect(orgCodeField).toBeInTheDocument();
+      expect(orgCodeField).toHaveValue("DMB");
+      expect(orgTranslationShortField).toBeInTheDocument();
+      expect(orgTranslationShortField).toHaveValue("Dean Map B");
+      expect(orgTranslationField).toBeInTheDocument();
+      expect(orgTranslationField).toHaveValue("Dean Map Bean");
+      expect(inactiveField).toBeInTheDocument();
+      expect(inactiveField).toHaveValue("false");
+
+      expect(submitButton).toHaveTextContent("Update");
+
+
+      fireEvent.change(orgTranslationShortField, {
+        target: { value: "Dean MAGE B" },
+      });
+      fireEvent.change(orgTranslationField, {
+        target: { value: "Dean MAGE Bean" },
+      });
+      fireEvent.change(inactiveField, {
+        target: { value: false },
+      });
+      fireEvent.click(submitButton);
+
+      await waitFor(() => expect(mockToast).toBeCalled());
+      expect(mockToast).toBeCalledWith(
+        "Organization Updated - orgCode: DMB orgTranslationShort: Dean MAGE B orgTranslation: Dean MAGE Bean inactive: false",
+      );
+
+      expect(mockNavigate).toBeCalledWith({ to: "/ucsborganization" });
+
+      expect(axiosMock.history.put.length).toBe(1); // times called
+      expect(axiosMock.history.put[0].params).toEqual({ orgCode: "DMB" });
+      expect(axiosMock.history.put[0].data).toBe(
+        JSON.stringify({
+          orgCode: "DMB",
+          orgTranslationShort: "Dean MAGE B",
+          orgTranslation: "Dean MAGE Bean",
+          inactive: "false",
+        }),
+      ); // posted object
+    });
+
+
   });
 });
