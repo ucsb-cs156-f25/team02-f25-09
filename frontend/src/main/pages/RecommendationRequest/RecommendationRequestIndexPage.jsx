fc4f4058bcf95f71304aa4218e165779abea65e8
Lauren Cho <laurencho@ucsb.edu>
2025-11-04 18:38:02 -0800

    messed up an import

--- frontend/src/main/pages/RecommendationRequest/RecommendationRequestIndexPage.jsx
+++ frontend/src/main/pages/RecommendationRequest/RecommendationRequestIndexPage.jsx
@@ -7,10 +7,10 @@ export default function RecommendationRequestIndexPage() {
       <div className="pt-2">
         <h1>Index page not yet implemented</h1>
         <p>
-          <a href="/placeholder/create">Create</a>
+          <a href="/recommendationrequest/create">Create</a>
         </p>
         <p>
-          <a href="/placeholder/edit/1">Edit</a>
+          <a href="/reommendationrequest/edit/1">Edit</a>
         </p>
       </div>
     </BasicLayout>
