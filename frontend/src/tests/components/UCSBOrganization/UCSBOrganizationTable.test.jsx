1c09a879302eaa5ac320d0d016c67a901db89c76
David Bazan <davidbazan@umail.ucsb.edu>
2025-11-04 17:39:52 -0800

    DB - add organization index page with stories and tests (Also fixed inconsistencies)

--- frontend/src/tests/components/UCSBOrganization/UCSBOrganizationTable.test.jsx
+++ frontend/src/tests/components/UCSBOrganization/UCSBOrganizationTable.test.jsx
@@ -234,6 +234,6 @@ describe("UCSBOrganizationTable tests", () => {
     // assert - check that the delete endpoint was called
 
     await waitFor(() => expect(axiosMock.history.delete.length).toBe(1));
-    expect(axiosMock.history.delete[0].params).toEqual({ id: "WPC" });
+    expect(axiosMock.history.delete[0].params).toEqual({ orgCode: "WPC" });
   });
 });
