61f84844196703a7239bded9a3ce19ef6100e4ea
Lauren Cho <laurencho@ucsb.edu>
2025-11-05 20:43:12 -0800

    fixed index

--- frontend/src/main/pages/RecommendationRequest/RecommendationRequestIndexPage.jsx
+++ frontend/src/main/pages/RecommendationRequest/RecommendationRequestIndexPage.jsx
@@ -1,17 +1,45 @@
+import React from "react";
+import { useBackend } from "main/utils/useBackend";
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import RecommendationRequestsTable from "main/components/RecommendationRequest/RecommendationRequestsTable";
+import { Button } from "react-bootstrap";
+import { useCurrentUser, hasRole } from "main/utils/useCurrentUser";
 
 export default function RecommendationRequestIndexPage() {
-  // Stryker disable all : placeholder for future implementation
+  const currentUser = useCurrentUser();
+
+  const createButton = () => {
+    if (hasRole(currentUser, "ROLE_ADMIN")) {
+      return (
+        <Button
+          variant="primary"
+          href="/recommendationrequests/create"
+          style={{ float: "right" }}
+        >
+          Create RecommendationRequest
+        </Button>
+      );
+    }
+  };
+
+  const {
+    data: recommendationRequests,
+    error: _error,
+    status: _status,
+  } = useBackend(
+    // Stryker disable next-line all : don't test internal caching of React Query
+    ["/api/recommendationrequests/all"],
+    { method: "GET", url: "/api/recommendationrequests/all" },
+    []
+  );
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p>
-          <a href="/recommendationrequest/create">Create</a>
-        </p>
-        <p>
-          <a href="/recommendationrequest/edit/1">Edit</a>
-        </p>
+        {createButton()}
+        <h1>RecommendationRequest</h1>
+        <RecommendationRequestsTable recommendationRequests={recommendationRequests} currentUser={currentUser} />
       </div>
     </BasicLayout>
   );
